a3519687619eca2460c5479ccb7c18ecadcef1c3
fabrizioPala <148344967+fabrizioPala@users.noreply.github.com>
2024-01-30 17:53:25 +0100

    Update and rename TodoList.jsx to Message.jsx

---
+++ myapp/src/Message.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+function Message() {
+  const isMounted = useRef(null);
+
+  useEffect(() => {
+    if (!isMounted.current) {
+      console.log("Mounting for the first time");
+      isMounted.current = true;
+    } else {
+      console.log("Mounting again for debug purposes");
+    };
+  }, []);
+  
+  return (
+    <div>
+      <p>useRefs</p>
+    </div>
+  );
+}
+
+export default Message;
